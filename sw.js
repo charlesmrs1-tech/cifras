776cfaba1d1205cf0881e60ea0de4f0ce39f1ef5
unknown <Administrador@premier.local>
2026-08-01 10:11:57 -0300

    fix: limpeza total de todas as sequencias do banco de dados Firebase v30

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "cifras-charles-v29";
+const CACHE_NAME = "cifras-charles-v30";
 const ASSETS = ["./", "./index.html", "./styles.css", "./app.js", "./firebase-config.js", "./manifest.webmanifest"];
 
 self.addEventListener("install", event => {
